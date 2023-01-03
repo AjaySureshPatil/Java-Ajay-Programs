8924d3249707b2120a828ffc3bde223c1a16b5ef
Ajaypatil8 <patilajay0035@gmail.com>
2023-01-03 17:54:53 +0530

    Commit

---
+++ TCS Digital Practice/Numbers/N23CheckNoisAutomatricOrNot.java
@@ -0,0 +1,26 @@
+public class N23CheckNoisAutomatricOrNot {
+
+    public static boolean isAutomorphic(int N) {
+        int sq = N * N;
+    
+        while (N > 0) {
+    
+          // Check if last digit is equal or not
+          if (N % 10 != sq % 10)
+            return false;
+    
+          // Reducing the number and its square
+          N /= 10;
+          sq /= 10;
+        }
+    
+        return true;
+      }
+      public static void main(String args[]) {
+        int n = 25;
+        if(isAutomorphic(n)==true)
+        System.out.println("Automorphic Number");
+        else
+        System.out.println("Not Automorphic Number");
+      }
+}
