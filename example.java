d1c8c0fef0849ad9d2b63f51780981c040496993
Ajaypatil8 <patilajay0035@gmail.com>
2023-01-11 15:46:01 +0530

    commit

---
+++ example.java
@@ -0,0 +1,16 @@
+public class example{
+
+    public static void bubblesort(int arr[]){
+        for(int turn=0; turn<arr.length-1; turn++){
+            for(int j=0; j<arr.length-1; j++){
+                
+            }
+        }
+    }
+    public static void main(String [] args){
+
+        int arr[] = {5,4,1,3,2};
+        bubblesort(arr);
+        printarr(arr);
+    }
+}
